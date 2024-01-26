8e489e30077ed3d1bb9c9b638bc6a0ebbdf301d8
Jeremy Ferguson <jmfergie@berkeley.edu>
2024-01-26 11:08:36 -0800

    changes to config file and parameters

---
+++ img_viewer_csv.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+import pandas as pd
+import os
+import json
+
+class ImageViewerApp:
+    def __init__(self, root, csv_file, root_directory, json_file):
+        self.root = root
+        self.root.title("Image Viewer App")
+        self.root.bind("<Left>", self.prev_image)
+        self.root.bind("<Right>", self.next_image)
+
+        self.image_index = 0
+        self.image_list = self.load_images_from_csv(csv_file, root_directory)
+        self.json_data = self.load_json_data(json_file)
+
+        self.display_image()
+
+    def load_images_from_csv(self, csv_file, root_directory):
+        df = pd.read_csv(csv_file)
+        image_list = [os.path.join(root_directory, fname) for fname in df['fname']]
+        return image_list
+
+    def load_json_data(self, json_file):
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+        return data
+
+    def display_image(self):
+        image_path = self.image_list[self.image_index]
+        img = Image.open(image_path)
+        img = img.resize((400, 400))
+        photo = ImageTk.PhotoImage(img)
+
+        if hasattr(self, 'label_image'):
+            self.label_image.configure(image=photo)
+            self.label_image.image = photo
+        else:
+            self.label_image = tk.Label(self.root, image=photo)
+            self.label_image.pack()
+
+        # Display current filename below the image
+        filename_label_text = os.path.basename(image_path)
+        if hasattr(self, 'label_filename'):
+            self.label_filename.configure(text=filename_label_text)
+        else:
+            self.label_filename = tk.Label(self.root, text=filename_label_text)
+            self.label_filename.pack()
+
+        # Display data associated with the filename
+        filename_key = filename_label_text
+        if filename_key in self.json_data:
+            data_label_text = f"Data: {self.json_data[filename_key]}"
+            if hasattr(self, 'label_data'):
+                self.label_data.configure(text=data_label_text)
+            else:
+                self.label_data = tk.Label(self.root, text=data_label_text)
+                self.label_data.pack()
+
+    def next_image(self, event):
+        self.image_index = (self.image_index + 1) % len(self.image_list)
+        self.display_image()
+
+    def prev_image(self, event):
+        self.image_index = (self.image_index - 1) % len(self.image_list)
+        self.display_image()
+
+if __name__ == "__main__":
+    csv_file = "partial_labeled_coco.csv"  # Replace with your CSV file path
+    root_directory = "/home/jmfergie/coco_imgs"  # Replace with your root directory path
+    json_file = "extracted_features_detr_500.json"  # Replace with your JSON file path
+
+    root = tk.Tk()
+    app = ImageViewerApp(root, csv_file, root_directory, json_file)
+    root.mainloop()
--- synth.py
+++ synth.py
@@ -18,19 +18,22 @@ class Program:
     def add_pred(self,pred):
         self.preds.append(pred)
 
-    def eval(self,doc):
+    def eval(self,doc,use_bins=True):
         score = 0
         if self.preds:
             for pred in self.preds:
                 if pred.eval(doc):
                     score += 1
             fraction = score / len(self.preds)
-            if fraction >= self.high_thresh:
-                return 2
-            elif fraction >= self.low_thresh:
-                return 1
+            if use_bins:
+                if fraction >= self.high_thresh:
+                    return 2
+                elif fraction >= self.low_thresh:
+                    return 1
+                else:
+                    return 0
             else:
-                return 0
+                return fraction
         else:
             return 0
 
@@ -156,10 +159,65 @@ def predGen(features,depth):
             for j, pred2 in enumerate(ZeroDepth()):
                 if i < j:
                     yield AndPred(pred1,pred2)
-                    yield OrPred(pred1, pred2)
+                    #yield OrPred(pred1, pred2)
     
+def mutual_info(pred1,pred2,top_docs,docs):
+    total = 0
+    covered = 0
+    if pred2 == None:
+        for fname in top_docs:
+            doc = docs[fname]
+            if pred1.eval(doc):
+                covered += 1
+            total += 1
+    else:
+        for fname in top_docs:
+            doc = docs[fname]
+            if pred1.eval(doc) or pred2.eval(doc):
+                covered += 1
+            total += 1
+    return covered / total  
+
+def filter_topk(k,topk,docs,program,top_docs,bottom_docs):
+    combined_pred = None
+    top_preds = []
+    all_preds = set()
+    while not topk.empty():
+        item_i = topk.get()
+        if item_i[3] < 0:
+            all_preds.add(Not(item_i[2]))
+        else:
+            all_preds.add(item_i[2])
+    for _ in range(k):
+        mis = {}
+        for pred in all_preds:
+            mi = mutual_info(pred,combined_pred,top_docs,docs)
+            mis[pred] = mi
+        max_pred = max(mis,key=lambda pred:mis[pred])
+        all_preds.remove(max_pred)
+        top_preds.append(max_pred)
+        if combined_pred:
+            combined_pred = OrPred(max_pred,combined_pred)
+        else:
+            combined_pred = max_pred
+    return top_preds
+
 def freq_heuristic(program, docs, features_set,examples, rejected_preds, config):
-    topk = PriorityQueue(config.predicates_per_round)
+    
+    top_docs = set()
+    bottom_docs = set()
+    for fname in docs:
+        doc = docs[fname]
+        doc_score = program.eval(doc)
+        if doc_score == 2:
+            top_docs.add(fname)
+        elif doc_score == 0:
+            bottom_docs.add(fname)
+    if config.use_mutual_information and len(top_docs) > 0:
+        size = config.mutual_info_pool_size
+    else:
+        size = config.predicates_per_round
+    topk = PriorityQueue(size)
     i = 0
     for pred in predGen(features_set,config.depth):
         i += 1
@@ -167,26 +225,53 @@ def freq_heuristic(program, docs, features_set,examples, rejected_preds, config)
             print(i)
         if pred in program or pred in rejected_preds or Not(pred) in program or Not(pred) in rejected_preds:
             continue
-        user_good = 0
-        nonuser_good = 0
-        user_bad = 0
-        nonuser_bad = 0
-        ex_fname = examples['fname'][0]
-        for fname in docs:
-            doc = docs[fname]
-            if pred.eval(doc):
-                if fname in examples['fname'].values:
-                    user_good += 1
+        
+        if len(program) == 0 or not config.use_bins or (len(top_docs) == 0 or len(bottom_docs) == 0):
+            user_true = 0
+            nonuser_true = 0
+            user_false = 0
+            nonuser_false = 0
+            for fname in docs:
+                doc = docs[fname]
+                if pred.eval(doc):
+                    if fname in examples['fname'].values:
+                        user_true += 1
+                    else:
+                        nonuser_true += 1
                 else:
-                    nonuser_good += 1
-            else:
-                if fname in examples['fname'].values:
-                    user_bad += 1
+                    if fname in examples['fname'].values:
+                        user_false += 1
+                    else:
+                        nonuser_false += 1
+            user_approved = user_true / (user_true + user_false)
+            full_docs = (user_true + nonuser_true) / (user_true + user_false + nonuser_true + nonuser_false)
+            score = user_approved - full_docs
+        else:
+            top_true = 0
+            top_false = 0
+            mid_true = 0
+            mid_false = 0
+            bottom_true = 0
+            bottom_false = 0
+            for fname in docs:
+                doc = docs[fname]
+                if pred.eval(doc):
+                    if fname in top_docs:
+                        top_true += 1
+                    elif fname in bottom_docs:
+                        bottom_true += 1
+                    else:
+                        mid_true += 1
                 else:
-                    nonuser_bad += 1
-        user_approved = user_good / (user_good + user_bad)
-        full_docs = (user_good + nonuser_good) / (user_good + user_bad + nonuser_good + nonuser_bad)
-        score = user_approved - full_docs
+                    if fname in top_docs:
+                        top_false += 1
+                    elif fname in bottom_docs:
+                        bottom_false += 1
+                    else:
+                        mid_false += 1
+            top_score = top_true / (top_true + top_false)
+            bottom_score = bottom_true / (bottom_true + bottom_false)
+            score = top_score - bottom_score
         item_i = (abs(score),i,pred,score)
         if not topk.full():
             topk.put(item_i)
@@ -194,22 +279,25 @@ def freq_heuristic(program, docs, features_set,examples, rejected_preds, config)
             item_k = topk.get()
             to_push = max(item_i,item_k,key=lambda item:item[0])
             topk.put(to_push)
-    preds = []
-    while not topk.empty():
-        item_i = topk.get()
-        print(item_i)
-        if item_i[3] < 0:
-            preds.append(Not(item_i[2]))
-        else:
-            preds.append(item_i[2])
-    return preds
+    if config.use_mutual_information and len(top_docs) > 0:
+        return filter_topk(config.predicates_per_round,topk,docs,program,top_docs,bottom_docs)
+    else:
+        preds = []
+        while not topk.empty():
+            item_i = topk.get()
+            print(item_i)
+            if item_i[3] < 0:
+                preds.append(Not(item_i[2]))
+            else:
+                preds.append(item_i[2])
+        return preds
 
 
 
 class AppConfig:
-    def __init__(self, low_threshold=0.2, high_threshold=0.8, features_fname=None, examples_fname=None,
-                 num_feature_selection_rounds=5, features_combine_count=5,depth=0,
-                 predicates_per_round=5, use_mutual_information=False,manual=False):
+    def __init__(self, low_threshold=0.2, high_threshold=0.8, features_fname=None, examples_fname=None, full_csv=None,
+                 num_feature_selection_rounds=5, features_combine_count=5,depth=0,mutual_info_pool_size=10,
+                 predicates_per_round=5, use_mutual_information=False,manual=False,use_bins = False, eval = False):
         # Validate thresholds
         assert 0 <= low_threshold <= 1, "Low threshold must be between 0 and 1."
         assert 0 <= high_threshold <= 1, "High threshold must be between 0 and 1."
@@ -225,6 +313,10 @@ class AppConfig:
         self.use_mutual_information = use_mutual_information
         self.manual = manual
         self.depth = depth
+        self.use_bins = use_bins
+        self.eval = eval
+        self.full_csv = full_csv
+        self.mutual_info_pool_size = mutual_info_pool_size
 
     @classmethod
     def from_yaml(cls, filename):
@@ -259,7 +351,16 @@ class App:
         else:
             self.runAutomatic()
         return self.prog
-
+    
+    def eval(self):
+        ground_truth = pd.read_csv(config.full_csv)
+        output = []
+        for fname in self.docs:
+            if fname in ground_truth['fname'].values:
+                expected_val = ground_truth[ground_truth['fname'] == fname]['val'].array[0]
+                pred_val = self.prog.eval(self.docs[fname],False)
+                output.append((fname,expected_val,pred_val))
+        return output
     def runAutomatic(self):
         for _ in range(self.config.num_feature_selection_rounds):
             new_preds = self.heuristic(self.prog,self.docs,self.all_features,self.user_examples,set(),self.config)
@@ -298,3 +399,5 @@ if __name__ == "__main__":
     app = App(freq_heuristic,config)
     prog = app.run()
     print("Final program: ",prog)
+    if config.eval:
+        app.eval()
