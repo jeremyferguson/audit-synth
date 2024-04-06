b4d805bc12c10a7a795bde89a2dd315584760b07
Justin Lubin <jlubi333@gmail.com>
2024-04-05 21:20:13 -0700

    Update plots

--- plots.py
+++ plots.py
@@ -5,15 +5,21 @@ matplotlib.use("Agg")
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 sns.set_theme(style="whitegrid")
-plt.rcParams["font.family"] = ["serif"]
-print(plt.rcParams["font.serif"])
-plt.rcParams["font.serif"] = "Times New Roman"
 title_size = 50
-ax_size = 46
-tick_size = 42
-fig_size = (14, 12)
+ax_size = 36
+tick_size = 30
+
+FIGSIZE = (25, 10)
+MARKERSIZE = 15
+CAPSIZE = 0  # 15
+CAPTHICK = 0  # 5
+MARKERS = ["o", "s", "^", "X"]
+COLORS = list(mcolors.TABLEAU_COLORS)
+LABELPAD = 20
+LINEWIDTH = 3
 
 
 def create_hist(df, params_dict, param_str, style):
@@ -108,14 +114,14 @@ def create_pareto_plot(results, param_dict, param_str, scatterplot=True):
     plt.ylim(0, 80)
     plt.legend()
     plt.xlabel("Number of Examples")
-    plt.ylabel(f"Decisions")
+    plt.ylabel(f"Number of Decisions")
     bins_str = "bins" if param_dict["use_bins"] else "no bins"
     mi_str = (
         f"mutual info with {param_dict['mi_pool']}k preds"
         if param_dict["use_mi"]
         else "no mutual info"
     )
-    plt.title(f"Decisions vs Number of Examples ")
+    # plt.title(f"Decisions vs Number of Examples ")
     plt.grid(True)
     plt.savefig(f"{param_str}_pareto.png")
     plt.clf()
@@ -123,7 +129,7 @@ def create_pareto_plot(results, param_dict, param_str, scatterplot=True):
     plt.ylim(-0.05, 1.05)
     plt.yticks([0.0, 0.25, 0.5, 0.75, 1.0])
     plt.xticks(range(0, 22, 2))
-    plt.title(f"Synthesis Completion Percentage vs Number of Examples")
+    # plt.title(f"Synthesis Completion Percentage vs Number of Examples")
     plt.ylabel("Fraction of Complete Runs")
     plt.xlabel("Number of Examples")
     plt.savefig(f"{param_str}_bar.png")
@@ -168,44 +174,87 @@ def create_highk_plot(results, param_dict, param_str, scatterplot=True):
     print(xs)
     print(percentage_complete)
     print(means)
-    lwidth = 4.0
     all_vals = np.array(all_vals)
     timeouts_df = pd.DataFrame(columns=["col", "y"], data=timeouts)
-    plt.figure(figsize=(14, 12))
-    plt.xticks(range(0, 330, 50), font={"size": tick_size})
-    plt.yticks(range(0, 35, 5), font={"size": tick_size})
+    plt.figure(figsize=FIGSIZE)
+    plt.xticks(range(0, 301, 20), font={"size": tick_size})
+    plt.yticks(range(0, 26, 5), font={"size": tick_size})
     plt.xlim(0, 310)
     # sns.lineplot(x=xs, y=means,color='b',label = "Successful Runs")
     if scatterplot and len(all_vals) > 0:
-        sns.plot(x=all_vals[:, :1], y=all_vals[:, 1:], linewidth=lwidth)
-    plt.errorbar(x=xs, y=means, yerr=stds, fmt="b", capsize=10, linewidth=lwidth)
+        plt.scatter(
+            all_vals[:, :1],
+            all_vals[:, 1:],
+            linewidth=LINEWIDTH,
+            color=COLORS[0],
+        )
+    plt.errorbar(
+        x=xs,
+        y=means,
+        yerr=stds,
+        capsize=CAPSIZE,
+        capthick=CAPTHICK,
+        linewidth=LINEWIDTH,
+        marker="o",
+        markersize=MARKERSIZE,
+        color=COLORS[0],
+    )
     # plot = sns.stripplot(data=timeouts_df,x='col',y='y',color='r',jitter=1.0,marker='^',label="Timeouts")
-    plt.ylim(0, 35)
+    plt.ylim(0, 25)
     # plt.legend()
-    plt.xlabel("Number of Examples", fontdict={"size": ax_size})
-    plt.ylabel(f"Decisions", fontdict={"size": ax_size})
+    plt.xlabel(
+        "Number of Examples",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
+    plt.ylabel(
+        f"Number of Decisions",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
     bins_str = "bins" if param_dict["use_bins"] else "no bins"
     mi_str = (
         f"mutual info with {param_dict['mi_pool']}k preds"
         if param_dict["use_mi"]
         else "no mutual info"
     )
-    plt.title(f"Decisions vs Number of Examples ", fontdict={"size": title_size})
+    # plt.title(f"Decisions vs Number of Examples ", fontdict={"size": title_size})
     plt.grid(True)
+    plt.tight_layout()
     plt.savefig(f"{param_str}_pareto.png")
     plt.clf()
-    plt.figure(figsize=(15, 12))
-    sns.lineplot(x=all_xs, y=percentage_complete, marker="o", linewidth=lwidth)
+    plt.figure(figsize=FIGSIZE)
+    plt.plot(
+        all_xs,
+        percentage_complete,
+        marker="o",
+        markersize=MARKERSIZE,
+        linewidth=LINEWIDTH,
+        color=COLORS[0],
+    )
     plt.ylim(-0.05, 1.05)
     plt.xlim(0, 310)
     plt.yticks([0.0, 0.25, 0.5, 0.75, 1.0], font={"size": tick_size})
-    plt.xticks(range(0, 330, 50), font={"size": tick_size})
-    plt.title(
-        f"Fraction of Runs that Reach 0.8 F1 Score", fontdict={"size": title_size}
+    plt.xticks(range(0, 301, 20), font={"size": tick_size})
+    # plt.title(
+    #     f"Fraction of Runs that Reach 0.8 F1 Score", fontdict={"size": title_size}
+    # )
+    plt.ylabel(
+        "Fraction Reaching 0.8 F1 Score",
+        fontdict={"size": ax_size},
+        labelpad=20,
+        fontweight="bold",
     )
-    plt.ylabel("Fraction", fontdict={"size": ax_size})
-    plt.xlabel("Number of Examples", fontdict={"size": ax_size})
+    plt.xlabel(
+        "Number of Examples",
+        fontdict={"size": ax_size},
+        labelpad=20,
+        fontweight="bold",
+    )
     # plt.legend(loc='lower right')
+    plt.tight_layout()
     plt.savefig(f"{param_str}_bar.png")
     plt.close()
 
@@ -214,139 +263,213 @@ def make_baseline_plots(
     baseline_scores, synth_scores: pd.DataFrame, params_dict, fname
 ):
     grouped = synth_scores.groupby("num_preds")
-    plt.figure(figsize=(15, 12))
-    lwidth = 3.0
+    plt.figure(figsize=FIGSIZE)
     labels = []
-    colors = ["blue", "blueviolet", "limegreen", "turquoise"]
     max_baseline_score = max(baseline_scores["f1"])
     baseline_scores["f1"] = max_baseline_score
     merged_df = pd.merge(synth_scores, baseline_scores, on="num_examples", how="left")
     merged_df["f1"] = merged_df["f1"].fillna(max_baseline_score)
     for i, (num_preds, score_group) in enumerate(grouped):
-        sns.lineplot(
-            data=score_group,
-            x="num_examples",
-            y="f1_mean",
-            color=colors[i % 4],
-            label=f"Synthesis with {num_preds} Decisions",
-            linewidth=lwidth,
-        )
+        # sns.lineplot(
+        #     data=score_group,
+        #     x="num_examples",
+        #     y="f1_mean",
+        #     color=COLORS[i],
+        #     linewidth=LINEWIDTH,
+        # )
         plt.errorbar(
             data=score_group,
             x="num_examples",
             y="f1_mean",
             yerr="f1_std",
-            fmt=colors[i % 4],
-            label="_",
-            linewidth=lwidth,
+            color=COLORS[i],
+            label=f"Synthesis with {num_preds} decisions",
+            linewidth=LINEWIDTH,
+            capsize=CAPSIZE,
+            capthick=CAPTHICK,
+            marker=MARKERS[i],
+            markersize=MARKERSIZE,
         )
-    sns.lineplot(
-        data=merged_df,
-        x="num_examples",
-        y="f1",
-        label="Baseline",
+    plt.plot(
+        # data=merged_df,
+        # x="num_examples",
+        # y="f1",
+        merged_df["num_examples"],
+        merged_df["f1"],
+        label="Baseline (LLM)",
         color="r",
-        linewidth=lwidth,
+        linewidth=LINEWIDTH,
     )
     labels.append("Baseline")
     plt.ylim(0, 1)
-    plt.xlim(-1, 31)
+    plt.xlim(0, 31)
     plt.yticks(np.linspace(0.0, 1.0, 5), font={"size": tick_size})
     plt.xticks(range(0, 32, 5), font={"size": tick_size})
-    plt.title(f"Synthesis Performance vs Baseline", fontdict={"size": title_size})
+    # plt.title(f"Synthesis Performance vs Baseline", fontdict={"size": title_size})
     plt.legend(loc="lower right", fontsize=34)
-    plt.ylabel("F1 Score", fontdict={"size": ax_size})
-    plt.xlabel("Number of Examples", fontdict={"size": ax_size})
+    plt.ylabel(
+        "F1 Score",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
+    plt.xlabel(
+        "Number of Examples",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
+    plt.tight_layout()
     plt.savefig(fname)
     plt.close()
 
 
 def make_ablation_plots(synth_scores: pd.DataFrame, fname):
     runs = synth_scores.groupby("label")
-    plt.figure(figsize=(15, 12))
+    plt.figure(figsize=FIGSIZE)
     labels = []
-    colors = ["blue", "blueviolet", "turquoise", "limegreen"]
-    lwidth = 3.0
     for i, (label, run) in enumerate(runs):
-        sns.lineplot(
-            data=run,
-            x="num_examples",
-            y="f1_mean",
-            color=colors[i % 4],
-            label=label,
-            linewidth=lwidth,
-        )
+        # sns.lineplot(
+        #     data=run,
+        #     x="num_examples",
+        #     y="f1_mean",
+        #     color=colors[i % 4],
+        #     label=label,
+        #     linewidth=LINEWIDTH,
+        # )
         plt.errorbar(
             data=run,
             x="num_examples",
             y="f1_mean",
             yerr="f1_std",
-            fmt=colors[i % 4],
-            label="_",
-            linewidth=lwidth,
+            # fmt=colors[i % 4],
+            color=COLORS[i],
+            label=label,
+            linewidth=LINEWIDTH,
+            marker=MARKERS[i],
+            markersize=MARKERSIZE,
         )
     plt.ylim(0, 1)
     plt.xlim(-1, 21)
     plt.yticks(np.linspace(0.0, 1.0, 5), font={"size": tick_size})
     plt.xticks(range(0, 22, 2), font={"size": tick_size})
-    plt.title(
-        f"Synthesis Performance for Ablated Parameters",
-        fontdict={"size": title_size},
-        y=1.04,
-    )
+    # plt.title(
+    #     f"Synthesis Performance for Ablated Parameters",
+    #     fontdict={"size": title_size},
+    #     y=1.04,
+    # )
     plt.legend(loc="lower right", fontsize=tick_size)
-    plt.ylabel("F1 Score", fontdict={"size": ax_size})
-    plt.xlabel("Number of Examples", fontdict={"size": ax_size})
-    # plt.tight_layout()
+    plt.ylabel(
+        "F1 Score",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
+    plt.xlabel(
+        "Number of Examples",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
+    plt.tight_layout()
     plt.savefig(fname)
     plt.close()
 
 
 def make_performance_plot(results: pd.DataFrame, fname, xs):
-    plt.figure(figsize=fig_size)
-    lwidth = 3.0
+    plt.figure(figsize=FIGSIZE)
     means = [results[results["prog_length"] == x].iloc[0]["time_mean"] for x in xs]
     stds = [results[results["prog_length"] == x].iloc[0]["time_std"] for x in xs]
-    plt.xlim(min(xs) - 1, max(xs) + 1)
-    sns.lineplot(x=xs, y=means, linewidth=lwidth, color="b")
-    plt.errorbar(x=xs, y=means, yerr=stds, linewidth=lwidth, capsize=10, color="b")
-    plt.title(f"Execution Time vs Program Size", fontdict={"size": title_size})
+    plt.xlim(min(xs), max(xs))
+    # sns.lineplot(x=xs, y=means, linewidth=LINEWIDTH, color="b")
+    plt.errorbar(
+        x=xs,
+        y=means,
+        yerr=stds,
+        linewidth=LINEWIDTH,
+        capsize=CAPSIZE,
+        color=COLORS[0],
+        capthick=CAPTHICK,
+        marker="o",
+        markersize=MARKERSIZE,
+    )
+    # plt.title(f"Execution Time vs Program Size", fontdict={"size": title_size})
     # plt.legend(loc = 'lower right')
-    plt.ylabel("Execution Time (seconds)", fontdict={"size": ax_size})
-    plt.xlabel("Program Size (# of predicates)", fontdict={"size": ax_size})
+    plt.ylabel(
+        "Execution Time (seconds)",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
+    plt.xlabel(
+        "Program Size (# of predicates)",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
     plt.xticks(range(0, 25, 5), font={"size": tick_size})
-    plt.yticks(np.linspace(1.5, 4.0, 6), font={"size": tick_size})
+    plt.yticks(np.arange(0, 4.1, 0.5), font={"size": tick_size})
+    plt.tight_layout()
     plt.savefig(fname)
     plt.close()
 
 
 def make_heuristic_plots(synth_scores: pd.DataFrame, fname):
-    plt.figure(figsize=(22, 12))
+    plt.figure(figsize=FIGSIZE)
     runs = synth_scores.groupby("label")
-    colors = ["blue", "blueviolet", "red", "limegreen"]
     for i, (label, run) in enumerate(runs):
-        sns.lineplot(
-            data=run, x="num_preds", y="f1_mean", color=colors[i % 4], label=label
-        )
+        # plt.scatter(
+        #    data=run,
+        #    x="num_preds",
+        #    y="f1_mean",
+        #    color=colors[i % 4],
+        #    label=label,
+        # )
         plt.errorbar(
             data=run,
             x="num_preds",
             y="f1_mean",
             yerr="f1_std",
-            fmt=colors[i % 4],
-            label="_",
+            fmt=COLORS[i],
+            label=label,
+            marker=MARKERS[i],
+            markersize=MARKERSIZE,
+            capsize=CAPSIZE,
+            capthick=CAPTHICK,
+            linewidth=LINEWIDTH,
         )
     plt.ylim(0, 1)
-    plt.xlim(-1, 101)
-    plt.yticks(np.linspace(0.0, 1.0, 5), font={"size": tick_size})
-    plt.xticks(range(0, 110, 10), font={"size": tick_size})
-    plt.title(
-        f"Synthesized Program Performance, by Predicate Selection Approach",
-        fontdict={"size": title_size},
-        y=1.04,
+    plt.xlim(0, 101)
+    plt.yticks(
+        np.linspace(0.0, 1.0, 5),
+        font={"size": tick_size},
     )
-    plt.legend(loc=(0, 0.15), fontsize=30)
-    plt.ylabel("F1 Score", fontdict={"size": ax_size})
-    plt.xlabel("Number of Decisions", fontdict={"size": ax_size})
+    plt.xticks(
+        range(0, 110, 10),
+        font={"size": tick_size},
+    )
+    # plt.title(
+    #     f"Synthesized Program Performance, by Predicate Selection Approach",
+    #     fontdict={"size": title_size},
+    #     y=1.04,
+    # )
+    plt.legend(
+        fontsize=30,
+        loc="upper left",
+        bbox_to_anchor=(0.01, 0.53),
+    )
+    plt.ylabel(
+        "F1 Score",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
+    plt.xlabel(
+        "Number of Decisions",
+        fontdict={"size": ax_size},
+        fontweight="bold",
+        labelpad=LABELPAD,
+    )
+    plt.tight_layout()
     plt.savefig(fname)
     plt.close()
